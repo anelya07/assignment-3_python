0b53056081eb121077f93ae8daf7d2452346e47d
anelya07 <253652@astanait.edu.kz>
2026-04-07 15:49:04 +0500

    task 3a

--- main.py
+++ main.py
@@ -1,57 +1,67 @@
 
-#TASK 3 - A
-
-customer = input("Enter customer name: ")
-check = True
-count = 0
-subtotal = 0
-
-while check == True:
-    product = input("Enter product name: ")
-    if product == "done":
-        check = False
-    else:
-        price = float(input("Enter price per unit (KZT): "))
-        count = count + 1
-        subtotal += price
+#TASK 3 - A    (ctrl + /) - comment
 
 print("=" * 30)
-print("         SHOP RECEIPT")
+
+store_info = ("FreshMart", "Astana, Respublika Ave 1", "+7 700 0000000")
+print(store_info[0])
+print(store_info[1])
+print(store_info[2])
+
 print("=" * 30)
 
-print(f"Customer: {customer.upper()}")
-print(f"Items: {count}")
-print(f"Subtotal: {subtotal}" + " KZT")
 
-print("-" * 30)
-
-if subtotal < 3000:
-    discount = 0.0
-    total = subtotal
-    print("Discount tier: No discount")
-    print(f"Discount: {discount}" + " KZT")
-    print(f"Total: {subtotal}" + " KZT")
-
-elif subtotal >= 3000 and subtotal < 7000:
-    discount = subtotal * 0.05
-    total = subtotal - discount
-    print("Discount tier: 5% discount")
-    print(f"Discount: {discount}" + " KZT")
-    print(f"Total: {total}" + " KZT")
-else:
-    discount = subtotal * 0.15
-    total = subtotal - discount
-    print("Discount tier: 15% discount")
-    print(f"Discount: {discount}" + " KZT")
-    print(f"Total: {total}" + " KZT")
-
-
-print("-" * 30)
-print("Name uppercase: ", customer.upper())
-print("Name lowercase: ", customer.lower())
-print("Name length: ", len(customer))
-if len(customer) > 5:
-    print("Long name")
-else:
-    print("Short name")
-print("=" * 30)
+# customer = input("Enter customer name: ")
+# check = True
+# count = 0
+# subtotal = 0
+#
+# while check == True:
+#     product = input("Enter product name: ")
+#     if product == "done":
+#         check = False
+#     else:
+#         price = float(input("Enter price per unit (KZT): "))
+#         count = count + 1
+#         subtotal += price
+#
+# print("=" * 30)
+# print("         SHOP RECEIPT")
+# print("=" * 30)
+#
+# print(f"Customer: {customer.upper()}")
+# print(f"Items: {count}")
+# print(f"Subtotal: {subtotal}" + " KZT")
+#
+# print("-" * 30)
+#
+# if subtotal < 3000:
+#     discount = 0.0
+#     total = subtotal
+#     print("Discount tier: No discount")
+#     print(f"Discount: {discount}" + " KZT")
+#     print(f"Total: {subtotal}" + " KZT")
+#
+# elif subtotal >= 3000 and subtotal < 7000:
+#     discount = subtotal * 0.05
+#     total = subtotal - discount
+#     print("Discount tier: 5% discount")
+#     print(f"Discount: {discount}" + " KZT")
+#     print(f"Total: {total}" + " KZT")
+# else:
+#     discount = subtotal * 0.15
+#     total = subtotal - discount
+#     print("Discount tier: 15% discount")
+#     print(f"Discount: {discount}" + " KZT")
+#     print(f"Total: {total}" + " KZT")
+#
+#
+# print("-" * 30)
+# print("Name uppercase: ", customer.upper())
+# print("Name lowercase: ", customer.lower())
+# print("Name length: ", len(customer))
+# if len(customer) > 5:
+#     print("Long name")
+# else:
+#     print("Short name")
+# print("=" * 30)
